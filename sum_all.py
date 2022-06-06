fdbe7a54a71cb5b44919d81756f44e645c4a9f65
Jalilov <javohirjalilov777@gmail.com>
2022-06-06 20:15:00 +0500

    added tasks

--- sum_all.py
+++ sum_all.py
@@ -8,11 +8,3 @@ def sum_all(arr: np.ndarray) -> int:
     Returns:
         int: sum of all numbers
     """
-    s = 0
-    for rows in arr:
-        for row in rows:
-            s += row
-
-    return s
-
-print(sum_all(np.arange(20).reshape(4,5)))
