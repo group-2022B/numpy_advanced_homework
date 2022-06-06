91bb7854c837f39f198d8923de8e63af445580b0
Jalilov <javohirjalilov777@gmail.com>
2022-06-06 19:16:29 +0500

    added task

---
+++ max_axis.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def sum_axis_1(arr: np.ndarray) -> int:
+    """
+    Returns the max of rows in the array
+    Args:
+        arr: np.ndarray
+    Returns:
+        np.ndarray: max of each row
+    """
